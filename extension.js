23b4a65f63b519de164304256a6cedb6fd19a437
ericuss <etorreg@gmail.com>
2020-06-18 18:18:54 +0200

    add some commands and tests

--- extension.js
+++ extension.js
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const ncp = require("copy-paste");
-// const clipboardy = require('clipboardy');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,42 +13,101 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "serializer" is now active!');
+	// console.log('Congratulations, your extension "serializer" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 
-	let serializeJsonToClipboardCommand = vscode.commands.registerCommand('serializer.serializeJsonToClipboard', function () {
-		vscode.window.showInformationMessage('The serialize and copy to clipboard process is executing');
-		let jsonActiveDocument = vscode.window.activeTextEditor.document.getText();
-		let stringifiedActiveDocument = JSON.stringify(jsonActiveDocument);
+	let serializeInitializeCommand = vscode.commands.registerCommand('serializer.serializeInitialize', async function () {
+		vscode.window.showInformationMessage('The serialize is active');
+	})
+	context.subscriptions.push(serializeInitializeCommand);
 
-		ncp.copy(stringifiedActiveDocument, function () {
-			// complete...
-			vscode.window.showInformationMessage('Copied to clipboard');
-		})
+	let serializeJsonToClipboardCommand = vscode.commands.registerCommand('serializer.serializeJsonToClipboard', async function () {
+		vscode.window.showInformationMessage('The serialize and copy to clipboard process is executing');
+		let textSerialized = getTextSeriarized();
+
+		await copyToClipboard(textSerialized);
 	})
 	context.subscriptions.push(serializeJsonToClipboardCommand);
 
 	let serializeJsonToFileCommand = vscode.commands.registerCommand('serializer.serializeJsonToFile', function () {
 		vscode.window.showInformationMessage('The serialize and copy to file process is executing');
-		let jsonActiveDocument = vscode.window.activeTextEditor.document.getText();
-		let stringifiedActiveDocument = JSON.stringify(jsonActiveDocument);
+		let textSerialized = getTextSeriarized();
 		let fileName = vscode.window.activeTextEditor.document.fileName;
 
-		if (fileName.indexOf('.json') > -1) {
-			fileName = fileName.replace('.json', '-serialized.txt');
-			let fs = require('fs');
-			fs.writeFile(fileName, stringifiedActiveDocument, function (err) {
-				if (err) return console.log(err);
-				vscode.window.showInformationMessage('Serialized into ' + fileName);
-			});
-		} else {
-			vscode.window.showErrorMessage('The extension of the file is not Json');
-		}
+		writeToFile(fileName, '-serialized.txt', textSerialized);
 	})
 	context.subscriptions.push(serializeJsonToFileCommand);
+
+	let serializeSelectedJsonToClipboardCommand = vscode.commands.registerCommand('serializer.serializeSelectedJsonToClipboard', async function () {
+		vscode.window.showInformationMessage('The serialize and copy to clipboard process is executing');
+		let textSerialized = getSelectedTextSerialized();
+
+		await copyToClipboard(textSerialized);
+	})
+	context.subscriptions.push(serializeSelectedJsonToClipboardCommand);
+
+	let serializeSelectedJsonToFileCommand = vscode.commands.registerCommand('serializer.serializeSelectedJsonToFile', function () {
+		vscode.window.showInformationMessage('The serialize and copy to file process is executing');
+		let textSerialized = getSelectedTextSerialized();
+		let fileName = vscode.window.activeTextEditor.document.fileName;
+
+		writeToFile(fileName, '-serializedSelectedText.txt', textSerialized);
+	})
+	context.subscriptions.push(serializeSelectedJsonToFileCommand);
+
+	let serializeSelectedJsonAndReplaceCommand = vscode.commands.registerCommand('serializer.serializeSelectedJsonAndReplace', async function () {
+		vscode.window.showInformationMessage('The serialize and copy to clipboard process is executing');
+		let textSerialized = getSelectedTextSerialized();
+
+		const selections = vscode.window.activeTextEditor.selections;
+
+		await vscode.window.activeTextEditor.edit(builder => {
+			for (const selection of selections) {
+				builder.replace(selection, textSerialized);
+			}
+		});
+	})
+	context.subscriptions.push(serializeSelectedJsonAndReplaceCommand);
+
+	function copyToClipboard(textSerialized) {
+		return new Promise((resolve, reject) => {
+			ncp.copy(textSerialized, function () {
+				vscode.window.showInformationMessage('Copied to clipboard');
+				return resolve();
+			});
+		})
+	}
+
+	function getSelectedTextSerialized() {
+		let selection = vscode.window.activeTextEditor.selection;
+		let selectedText = vscode.window.activeTextEditor.document.getText(selection);
+		let stringifiedActiveDocument = JSON.stringify(JSON.parse(selectedText));
+		return stringifiedActiveDocument;
+	}
+
+	function getTextSeriarized() {
+		let jsonActiveDocument = vscode.window.activeTextEditor.document.getText();
+		let stringifiedActiveDocument = JSON.stringify(JSON.parse(jsonActiveDocument));
+		return stringifiedActiveDocument;
+	}
+
+	function writeToFile(fileName, fileNameSufix, text) {
+		if (fileName.indexOf('.json') > -1) {
+			fileName = fileName.replace('.json', fileNameSufix);
+			let fs = require('fs');
+			fs.writeFile(fileName, text, function (err) {
+				if (err)
+					return console.log(err);
+				vscode.window.showInformationMessage('Serialized into ' + fileName);
+			});
+		}
+		else {
+			vscode.window.showErrorMessage('The extension of the file is not Json');
+		}
+	}
 
 	// let serializerCommand = vscode.commands.registerCommand('serializer.serializer', function () {
 	// 	// The code you place here will be executed every time your command is executed
--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,15 +1,119 @@
 const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+const path = require('path');
 const vscode = require('vscode');
-// const myExtension = require('../extension');
+const ncp = require("copy-paste");
 
+const Extension = require('../../extension');
+const testFolderLocation = '/../../test/test-environment/'
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
+	const expected_json_serialized = '{"quiz":{"sport":{"q1":{"question":"Which one is correct team name in NBA?","options":["New York Bulls","Los Angeles Kings","Golden State Warriros","Huston Rocket"],"answer":"Huston Rocket"}},"maths":{"q1":{"question":"5 + 7 = ?","options":["10","11","12","13"],"answer":"12"},"q2":{"question":"12 - 8 = ?","options":["1","2","3","4"],"answer":"4"}}}}'
+	const expected_selected_json_serialized = '{"q1":{"question":"Which one is correct team name in NBA?","options":["New York Bulls","Los Angeles Kings","Golden State Warriros","Huston Rocket"],"answer":"Huston Rocket"}}'
 
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
 	});
+
+	// the first call to extension cost a more than 1s
+	test('Initialize extensions', async () => {
+		await vscode.commands.executeCommand('serializer.serializeInitialize')
+		assert.equal(true, true)
+	})
+
+	// from file
+	test('Given a json file when call to serialize json to clipboard then the clipboard contains the serialized json', async () => {
+		await openSampleFile()
+		await vscode.commands.executeCommand('serializer.serializeJsonToClipboard')
+		const text = getTextFromClipboard();
+
+		assert.equal(text, expected_json_serialized);
+	})
+
+	test('Given a json file when call to serialize json to file then the file is generated and contains the serialized json', async () => {
+		await openSampleFile()
+		await vscode.commands.executeCommand('serializer.serializeJsonToFile')
+		const text = await getTextFromGeneratedFile('test-serialized.txt');
+
+		assert.equal(text, expected_json_serialized);
+		deleteFile(path.join(__dirname + testFolderLocation + 'test-serialized.txt'));
+	})
+
+	// from selected text
+	test('Given a selected text inside editor when call to serialize selected json to clipboard then the clipboard contains the serialized json', async () => {
+		await openSampleFile()
+
+		var selection = new vscode.Selection(2, 17, 13, 9);
+		vscode.window.activeTextEditor.selection = selection;
+
+		vscode.window.activeTextEditor.selections = [selection];
+
+		await vscode.commands.executeCommand('serializer.serializeSelectedJsonToClipboard')
+		const text = getTextFromClipboard();
+
+		assert.equal(text, expected_selected_json_serialized);
+	})
+
+	test('Given a selected text inside editor when call to serialize selected json to file then the file contains the serialized json', async () => {
+		await openSampleFile()
+
+		var selection = new vscode.Selection(2, 17, 13, 9);
+		vscode.window.activeTextEditor.selection = selection;
+
+		vscode.window.activeTextEditor.selections = [selection];
+
+		await vscode.commands.executeCommand('serializer.serializeSelectedJsonToFile')
+
+		const text = await getTextFromGeneratedFile('test-serializedSelectedText.txt');
+		assert.equal(text, expected_selected_json_serialized);
+		await deleteFile(path.join(__dirname + testFolderLocation + 'test-serializedSelectedText.txt'));
+	})
+
+	test('Given a selected text inside editor when call to serialize selected json and replace then editor contains the serialized json replacing the old selected json', async () => {
+		await openSampleFile()
+
+		var selection = new vscode.Selection(2, 17, 13, 9);
+		vscode.window.activeTextEditor.selection = selection;
+
+		vscode.window.activeTextEditor.selections = [selection];
+
+		await vscode.commands.executeCommand('serializer.serializeSelectedJsonAndReplace')
+		let text = getSelectedTextSerialized()
+		assert.equal(text, expected_selected_json_serialized);
+	})
+
 });
+
+function getSelectedTextSerialized() {
+	let selection = vscode.window.activeTextEditor.selection;
+	let selectedText = vscode.window.activeTextEditor.document.getText(selection);
+	let stringified = JSON.stringify(JSON.parse(selectedText));
+	return stringified;
+}
+
+function getTextFromClipboard() {
+	return ncp.paste()
+}
+
+async function getTextFromGeneratedFile(fileName) {
+	const uriTxt = vscode.Uri.file(
+		path.join(__dirname + testFolderLocation + fileName)
+	);
+	const txtDocument = await vscode.workspace.openTextDocument(uriTxt);
+	await vscode.window.showTextDocument(txtDocument);
+	const text = vscode.window.activeTextEditor.document.getText();
+	return text;
+}
+
+async function deleteFile(fileName) {
+	let fs = require('fs');
+	await fs.unlink(fileName, () => { })
+}
+
+async function openSampleFile() {
+	const uri = vscode.Uri.file(
+		path.join(__dirname + testFolderLocation + 'test.json')
+	)
+	const jsonDocument = await vscode.workspace.openTextDocument(uri)
+	await vscode.window.showTextDocument(jsonDocument)
+}
